9e35366b5b3dfc7ed61cb7f6d70631c9dac430fd
Zana <zanna._@hotmail.com>
2020-07-18 00:52:26 +0200

    Krijimi i file-it the ri

---
+++ MusicApp/PlayerViewController.swift
@@ -0,0 +1,27 @@
+//
+//  PlayerViewController.swift
+//  MusicApp
+//
+//  Created by Zana Ramizi on 7/18/20.
+//  Copyright © 2020 Zana Ramizi. All rights reserved.
+//
+
+import UIKit
+
+class PlayerViewController: UIViewController {
+
+    public var position: Int = 0
+    public var songs: [Song] = []
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    
+
+   
+
+}
--- MusicApp/ViewController.swift
+++ MusicApp/ViewController.swift
@@ -88,6 +88,11 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         return cell
         
     }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+        
+    }
 
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
