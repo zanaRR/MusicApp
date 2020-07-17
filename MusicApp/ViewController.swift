34d28ef32297359b482fd263721f945be77a63c7
Zana <zanna._@hotmail.com>
2020-07-17 20:07:15 +0200

    Krijimi i funksioneve

--- MusicApp/ViewController.swift
+++ MusicApp/ViewController.swift
@@ -8,18 +8,59 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
 
+    
+    @IBOutlet var table: UITableView!
+    
+    var songs = [Song]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        configureSongs()
+        table.delegate = self
+         table.dataSource = self
+        
+    }
+    func configureSongs() {
+        songs.append(Song(name: String,
+                          albumName: String,
+                          artistName: String,
+                          imageName: String,
+                          trackName: String))
+    }
+    //Table
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return songs.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: IndexPath)
+        // configure
+        return cell
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: IndexPath, animated: true)
+        
+        //present the player
+        let position = indexPath.row
+        //songs
+        guard let vc = storyboard?.instantiateViewController(withIdentifier: "player")else {
+            return
+        }
+        
+        present(vc, animated: true)
     }
-
-
+    
+}
+struct Song {
+    let name: String
+    let albumName: String
+    let artistName: String
+    let imageName: String
+    let trackName: String
+    
 }
 
