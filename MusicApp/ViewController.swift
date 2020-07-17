06c1bb308455e768e449ca7a4e4002c3692d8047
Zana <zanna._@hotmail.com>
2020-07-17 23:44:32 +0200

    Krijimi i funksioeve

--- MusicApp/ViewController.swift
+++ MusicApp/ViewController.swift
@@ -23,26 +23,74 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
     }
     func configureSongs() {
-        songs.append(Song(name: String,
-                          albumName: String,
-                          artistName: String,
-                          imageName: String,
-                          trackName: String))
-    }
-    //Table
+        songs.append(Song(name: "Bailando",
+                          albumName: "Bailandos",
+                          artistName: "Enrique Iglesias",
+                          imageName: "cover1",
+                          trackName: "song1"))
+        songs.append(Song(name: "Vamos pa la playa",
+                          albumName: "Playa",
+                          artistName: "Pedro Capo",
+                          imageName: "cover2",
+                          trackName: "song3"))
+        songs.append(Song(name: "Savage Love",
+                          albumName: "Savage",
+                          artistName: "Jason Derulo",
+                          imageName: "cover3",
+                          trackName: "song3"))
+        songs.append(Song(name: "Bailando",
+                          albumName: "Bailandos",
+                          artistName: "Enrique Iglesias",
+                          imageName: "cover1",
+                          trackName: "song1"))
+        songs.append(Song(name: "Vamos pa la playa",
+                          albumName: "Playa",
+                          artistName: "Pedro Capo",
+                          imageName: "cover2",
+                          trackName: "song3"))
+        songs.append(Song(name: "Savage Love",
+                          albumName: "Savage",
+                          artistName: "Jason Derulo",
+                          imageName: "cover3",
+                          trackName: "song3"))
+        songs.append(Song(name: "Bailando",
+                          albumName: "Bailandos",
+                          artistName: "Enrique Iglesias",
+                          imageName: "cover1",
+                          trackName: "song1"))
+        songs.append(Song(name: "Vamos pa la playa",
+                          albumName: "Playa",
+                          artistName: "Pedro Capo",
+                          imageName: "cover2",
+                          trackName: "song3"))
+        songs.append(Song(name: "Savage Love",
+                          albumName: "Savage",
+                          artistName: "Jason Derulo",
+                          imageName: "cover3",
+                          trackName: "song3"))     }
+   //Table
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return songs.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: IndexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let song = songs[indexPath.row]
+        
         // configure
+        cell.textLabel?.text = song.name
+        cell.detailTextLabel?.text = song.albumName
+        cell.accessoryType = .disclosureIndicator
+        cell.imageView?.image = UIImage(named: song.imageName)
+        cell.textLabel?.font = UIFont(name: "Helvetica-Bold" , size: 18)
+        cell.detailTextLabel?.font = UIFont(name: "Helvetica" , size: 17 )
+        
         return cell
         
     }
 
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: IndexPath, animated: true)
+        tableView.deselectRow(at: indexPath, animated: true)
         
         //present the player
         let position = indexPath.row
