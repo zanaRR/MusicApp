9103b15bc720db2b56a2c6e7444a424b7b7c6fe9
Zana <zanna._@hotmail.com>
2020-07-18 17:00:13 +0200

    Styling

--- MusicApp/PlayerViewController.swift
+++ MusicApp/PlayerViewController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2020 Zana Ramizi. All rights reserved.
 //
 
+import AVFoundation
 import UIKit
 
 class PlayerViewController: UIViewController {
@@ -16,6 +17,38 @@ class PlayerViewController: UIViewController {
     
     @IBOutlet var holder: UIView!
     
+    var player: AVAudioPlayer?
+    
+    // User Interface elements
+    
+    private let albumImageView: UIImageView = {
+       let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    private let songNameLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.numberOfLines = 0 // line wrap
+        return label
+    }()
+    
+    private let artistNameLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.numberOfLines = 0 // line wrap
+        return label
+    }()
+    
+    private let albumNameLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.numberOfLines = 0 // line wrap
+        return label
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -24,7 +57,7 @@ class PlayerViewController: UIViewController {
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        if holder.subviews.count == 1 {
+        if holder.subviews.count == 0 {
             configure()
     }
 }
@@ -32,8 +65,116 @@ class PlayerViewController: UIViewController {
     func configure() {
         // set up player
         
-        //set up user interface elements 
+        let song = songs[position]
         
-     }
+        let urlString = Bundle.main.path(forResource: song.trackName, ofType: "mp3")
+        
+        do {
+            
+          try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback, mode: AVAudioSessionModeDefault, options: [.mixWithOthers, .allowAirPlay])
+          try  AVAudioSession.sharedInstance().setActive(true, with: .notifyOthersOnDeactivation)
+            
+            guard let urlString = urlString else {
+                
+                print("urlstring is nil")
+                return
+            }
+            
+            player = try AVAudioPlayer(contentsOf: URL(string: urlString)!)
+            
+            guard let player = player else {
+                print("player is nil")
+                return
+                
+            }
+            player.volume = 0.5
+            
+            player.play()
+            
+        } catch {
+            print("error occurrred")
+        }
+        
+        //set up user interface elements
+        
+        //album cover
+        albumImageView.frame = CGRect(x: 10,
+                                      y: 10,
+                                      width: holder.frame.size.width-20,
+                                      height: holder.frame.size.width-20)
+        
+        albumImageView.image = UIImage(named: song.imageName)
+        holder.addSubview(albumImageView)
+        
+        // Labels: Song name, album, artist
+        songNameLabel.frame = CGRect(x: 10,
+                                      y: albumImageView.frame.size.height + 10,
+                                      width: holder.frame.size.width-20,
+                                      height: 70)
+        
+        albumNameLabel.frame = CGRect(x: 10,
+                                      y:albumImageView.frame.size.height + 10 + 70,
+                                      width: holder.frame.size.width-20,
+                                      height: 70)
+        
+        artistNameLabel.frame = CGRect(x: 10,
+                                      y: albumImageView.frame.size.height + 10 + 140,
+                                      width: holder.frame.size.width-20,
+                                      height: 70)
+        
+        songNameLabel.text = song.name
+        albumNameLabel.text = song.albumName
+        artistNameLabel.text = song.artistName
+        
+        holder.addSubview(songNameLabel)
+        holder.addSubview(albumNameLabel)
+        holder.addSubview(artistNameLabel)
+        
+        //Player controls
+        let playPauseButton = UIButton()
+        let nextButton = UIButton()
+        let backButton = UIButton()
+        
+        
+        
+        //Styling
+        playPauseButton.setBackgroundImage(UIImage(systemName: "pause.fill"), for: .normal)
+        backButton.setBackgroundImage(UIImage(systemName: "back.fill"), for: .normal)
+        nextButton.setBackgroundImage(UIImage(systemName: "forward.fill"), for: .normal)
+        
+        playPauseButton.tintColor = .black
+        backButton.tintColor = .black
+        nextButton.tintColor = .black
+        
+        holder.addSubview(playPauseButton)
+        holder.addSubview(nextButton)
+        holder.addSubview(backButton)
+        
+        //slider
+        let slider = UISlider(frame: CGRect(x: 20,
+                                            y: holder.frame.size.height-60,
+                                            width: holder.frame.size.width-40,
+                                            height: 50))
+        
+        
+        slider.value = 0.5
+        slider.addTarget(self, action: #selector(didSlideSlider(_:)), for: .valueChanged)
+        holder.addSubview(slider)
+    }
+    
+    @objc func didSlideSlider(_ slider: UISlider) {
+        let value = slider.value
+        player?.volume = value
+        
+      
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        if let player = player {
+            player.stop()
+        }
+    }
 }
 
